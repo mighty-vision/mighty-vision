1b907471f08811dea90df693c89fb34fc3325d27
Sergey Pimenov <pimenov.web@gmail.com>
2018-12-10 20:29:46 +0300

    feat: add a11y for "scroll to content"

---
+++ src/basic/scripts/scrollToContent.js
@@ -0,0 +1,56 @@
+import initScreen from './../../components/initScreen/initScreen';
+
+var initScreenNode = document.querySelector('.initScreen');
+
+var scroll = {
+  onContent : false,
+
+  init() {
+    scroll.setListeners();
+  },
+
+  setListeners() {
+    initScreenNode.addEventListener('initScreenAnimationEnd', () => { 
+      document.addEventListener('wheel', () => {
+        scroll.toContent();
+      });
+
+      document.addEventListener('keydown', (e) => {
+        if(e.keyCode == 40 || e.keyCode == 34 || e.keyCode == 32) {
+          scroll.toContent();
+        }
+      });
+    });
+
+    document.addEventListener('keydown', (e) => {
+      if(!scroll.onContent && e.keyCode == 9) {
+        e.preventDefault();
+      }
+    });
+  },
+
+  toContent() {
+    if(scroll.onContent) return;
+    
+    scroll.onContent = true;
+
+    var scrollTime = 600;
+    
+    smoothScroll.scrollTo(768, scrollTime, document.body);
+  
+    setTimeout(() => {
+      initScreen.hide();
+  
+      document.body.scroll({
+        top: 0, 
+        left: 0
+      });
+  
+      document.body.classList.add('enableScroll');
+
+      document.body.focus();
+    }, scrollTime + 100);
+  }
+}
+
+export default scroll;

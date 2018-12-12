fd53b44bb3f46ec76e263b036ce8777ec6c616af
Sergey Pimenov <pimenov.web@gmail.com>
2018-12-12 18:04:37 +0300

    feat: adapt init screen

--- src/components/initScreen/initScreen.js
+++ src/components/initScreen/initScreen.js
@@ -2,16 +2,24 @@ var initScreenNode = document.querySelector('.initScreen');
 var videoNode = document.querySelector('.initScreen .video');
 var contentNode = document.querySelector('.initScreen .content');
 
+var mobile = (window.innerWidth <= 768);
+
 var initScreen = {
+  videoPlayed : false,
+
   init() {
     initScreen.setupVideo();
     initScreen.setListeners();
   },
 
   setListeners() {
+    window.addEventListener('load', () => {
+      document.body.classList.add('startAnimation');
+    });
+
+
     videoNode.addEventListener('animationend', () => {
       initScreen.playVideo();
-      initScreen.showAnimation();
     });
 
     contentNode.addEventListener('animationend', () => {
@@ -23,26 +31,26 @@ var initScreen = {
   setupVideo() {
     var mobilePreffix = '';
 
-    if(window.innerWidth <= 768) {
+    if(mobile) {
       mobilePreffix = '-mobile'
     }
 
     var videoSrc = videoNode.dataset.src;
 
-    videoNode.innerHTML = `<source src="${videoSrc}${mobilePreffix}.mp4" 
+    videoNode.innerHTML = `<source src="${videoSrc}${mobilePreffix}.mp4#t=0.1" 
                                    type="video/mp4; codecs="&quot";avc1.42E01E, mp4a.40.2&quot;">`;
   },
 
   playVideo() {
+    if(initScreen.videoPlayed) return;
+
+    initScreen.videoPlayed = true;
+
     videoNode.play();
   },
 
   hide() {
     initScreenNode.classList.add('hide');
-  },
-
-  showAnimation() {
-    initScreenNode.classList.add('showAnimation');
   }
 }
 

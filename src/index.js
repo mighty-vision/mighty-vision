07acf43a43c62ec411e1b2f12cb7512587694c82
Sergey Pimenov <pimenov.web@gmail.com>
2018-12-12 18:09:52 +0300

    feat: update JS imports and locales

--- src/index.js
+++ src/index.js
@@ -7,17 +7,30 @@ import focusOnlyWithKeys from './utils/scripts/focusOnlyWithKeys';
 
 // Basic
 import scroll from './basic/scripts/scrollToContent';
+import scrollAnimation from './basic/scripts/scrollAnimation';
 
 // Components
 import initScreen from './components/initScreen/initScreen';
+import contacts from './components/contacts/contacts';
+
 
 detectBrowser();
 focusOnlyWithKeys();
 initScrollLib();
 initScreen.init();
+contacts.init();
+
+
+var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+
+if(isMac) {
+	document.getElementsByTagName('html')[0].classList.add('mac')
+}
 
 var isMobile = (window.innerWidth <= 768);
 
 if(document.body.dataset.browser != 'edge' && !isMobile) {
   scroll.init();
 }
+
+if (isMobile) scrollAnimation();

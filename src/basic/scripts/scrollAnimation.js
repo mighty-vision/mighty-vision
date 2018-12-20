047c1a071c01090b317ae049969f63775668e53d
Sergey Pimenov <pimenov.web@gmail.com>
2018-12-20 21:29:29 +0300

    feat : redesign animation

--- src/basic/scripts/scrollAnimation.js
+++ src/basic/scripts/scrollAnimation.js
@@ -1,23 +1,35 @@
 import setCSSVar from './../../utils/scripts/setCSSVar';
 
+var bodyHeight = window.innerHeight;
+
 export default function() {
 	var controller = new ScrollMagic.Controller();
 	
-	var distance0 = window.innerHeight * 1.4;
+	var distance0 = bodyHeight * 1.4;
 
 	new ScrollMagic.Scene({
 			duration: distance0,
-			offset: window.innerHeight * 0.2
+			offset: 0
 		})
 		.on("progress", function (e) {
 			setCSSVar(document.body, 'progress-one', e.progress);
 		})
 		.addTo(controller);
 
+	new ScrollMagic.Scene({
+		duration: 150,
+		offset: bodyHeight * 0.6
+	})
+	.on("progress", function (e) {
+		setCSSVar(document.body, 'progress-two', e.progress);
+	})
+	.addTo(controller);
+
+
 	var start2NodeData = document.querySelector('.secondScreen .content').getBoundingClientRect();
 	var startPos2 = start2NodeData.height * 1.5;
 
-	var distance = window.innerHeight * 1.2;
+	var distance = bodyHeight * 1.2;
 
 	new ScrollMagic.Scene({
 			duration: distance,
@@ -32,4 +44,30 @@ export default function() {
 			setCSSVar(document.body, 'progress', e.progress);
 		})
 		.addTo(controller);
+
+
+	var startPos3 = document.querySelector('.thirdScreen .title').getBoundingClientRect().top - bodyHeight;
+
+	new ScrollMagic.Scene({
+		duration: 150,
+		offset: startPos3
+	})
+	.on("progress", function (e) {
+		setCSSVar(document.body, 'progress-three', e.progress);
+	})
+	.addTo(controller);
+
+
+	var startPos4 = document.querySelector('.thirdScreen .methodology').getBoundingClientRect().top - bodyHeight + 200;
+
+	new ScrollMagic.Scene({
+		duration: 150,
+		offset: startPos4
+	})
+	.on("progress", function (e) {
+		setCSSVar(document.body, 'progress-four', e.progress);
+
+		console.log('hui 4')
+	})
+	.addTo(controller);
 }

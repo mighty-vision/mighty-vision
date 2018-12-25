2377400a059f2bfe3bb91715ff78351edc84b28a
Sergey Pimenov <pimenov.web@gmail.com>
2018-12-25 21:58:34 +0300

    feat (mobile): change scroll animation

--- src/basic/scripts/scrollAnimation.js
+++ src/basic/scripts/scrollAnimation.js
@@ -1,19 +1,27 @@
 import setCSSVar from './../../utils/scripts/setCSSVar';
 
-var bodyHeight = window.innerHeight;
-var initScreenTitleNode = document.querySelector('.initScreen .title');
+var initScreenDescriptionNode = document.querySelector('.initScreen .description');
 var secondScreenTitleNode = document.querySelector('.secondScreen .title');
-var thirdScreenTitleNode = document.querySelector('.thirdScreen .title');
+var secondScreenDescriptionNode = document.querySelector('.secondScreen .description');
 var secondScreenNode = document.querySelector('.secondScreen');
+var thirdScreenTitleNode = document.querySelector('.thirdScreen .title');
+var thirdScreenDescriptionNode = document.querySelector('.thirdScreen .description');
 
-export default function() {
+var bodyHeight = window.innerHeight;
+
+export default function scrollAniation() {
+	if(document.body.dataset.browser == 'safari') {
+		bodyHeight += 69; // Height without top & bottom bars for iOS
+	} else {
+		bodyHeight += 50;
+	}
+
 	var controller = new ScrollMagic.Controller();
 
-	var start1 = initScreenTitleNode.getBoundingClientRect().top;
-	var baseDistance = 200;
+	var start1 = initScreenDescriptionNode.getBoundingClientRect().top;
 
 	new ScrollMagic.Scene({
-		duration: baseDistance,
+		duration: initScreenDescriptionNode.getBoundingClientRect().height,
 		offset: start1
 	})
 	.on("progress", function (e) {
@@ -21,11 +29,15 @@ export default function() {
 	})
 	.addTo(controller);
 
+	document.addEventListener('click', () => {
+		alert(window.innerHeight)
+	});
 
-	var start2 = secondScreenNode.getBoundingClientRect().top - bodyHeight;
+
+	var start2 = secondScreenNode.getBoundingClientRect().top - bodyHeight - 100;
 
 	new ScrollMagic.Scene({
-		duration: bodyHeight,
+		duration: bodyHeight + 100,
 		offset: start2
 	})
 	.on("progress", function (e) {
@@ -37,19 +49,21 @@ export default function() {
 	var start3 = secondScreenTitleNode.getBoundingClientRect().top - bodyHeight;
 
 	new ScrollMagic.Scene({
-		duration: baseDistance,
+		duration: 70,
 		offset: start3
 	})
 	.on("progress", function (e) {
 		setCSSVar(document.body, 'progress-3', e.progress);
+		console.log('wrk')
 	})
 	.addTo(controller);
 
 
-	var start4 = secondScreenTitleNode.getBoundingClientRect().top - 120;
+	var start4 = secondScreenDescriptionNode.getBoundingClientRect().top + 
+							 secondScreenDescriptionNode.getBoundingClientRect().height / 2;
 
 	new ScrollMagic.Scene({
-		duration: baseDistance,
+		duration: secondScreenDescriptionNode.getBoundingClientRect().height / 2,
 		offset: start4
 	})
 	.on("progress", function (e) {
@@ -61,7 +75,7 @@ export default function() {
 	var start5 = secondScreenNode.getBoundingClientRect().bottom - bodyHeight;
 
 	new ScrollMagic.Scene({
-		duration: bodyHeight,
+		duration: bodyHeight + 200,
 		offset: start5
 	})
 	.on("progress", function (e) {
@@ -73,12 +87,22 @@ export default function() {
 	var start6 = thirdScreenTitleNode.getBoundingClientRect().top - bodyHeight;
 
 	new ScrollMagic.Scene({
-		duration: baseDistance,
+		duration: 70,
 		offset: start6
 	})
 	.on("progress", function (e) {
 		setCSSVar(document.body, 'progress-6', e.progress);
-		console.log('wrk')
+	})
+	.addTo(controller);
+
+	var start7 = thirdScreenDescriptionNode.getBoundingClientRect().top;
+
+	new ScrollMagic.Scene({
+		duration: thirdScreenDescriptionNode.getBoundingClientRect().height,
+		offset: start7
+	})
+	.on("progress", function (e) {
+		setCSSVar(document.body, 'progress-7', e.progress);
 	})
 	.addTo(controller);
 }
